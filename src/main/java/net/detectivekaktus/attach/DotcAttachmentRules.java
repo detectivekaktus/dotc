ad23754ae00f7b45c7207642eaf331ebfc46d5fa
Artiom Astashonak <artiomastashonak@gmail.com>
2026-04-10 23:10:42 +0200

    attach: add `Stats` attachment

--- src/main/java/net/detectivekaktus/attach/DotcAttachmentRules.java
+++ src/main/java/net/detectivekaktus/attach/DotcAttachmentRules.java
@@ -3,4 +3,8 @@ package net.detectivekaktus.attach;
 public class DotcAttachmentRules {
     public static final float DEFAULT_MAX_MANA = 200.0f;
     public static final float MIN_MANA = 0.0f;
+
+    public static final int DEFAULT_STRENGTH = 0;
+    public static final int DEFAULT_AGILITY = 0;
+    public static final int DEFAULT_INTELLIGENCE = 0;
 }
