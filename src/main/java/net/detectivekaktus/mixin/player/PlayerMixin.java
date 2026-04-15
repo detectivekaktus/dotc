d102144443a1cf711c962042a3cdde9506e320ef
Artiom Astashonak <artiomastashonak@gmail.com>
2026-04-15 17:19:34 +0200

    fix: don't trigger `PlayerMixin.onAttack()` when target is invulnerable

--- src/main/java/net/detectivekaktus/mixin/player/PlayerMixin.java
+++ src/main/java/net/detectivekaktus/mixin/player/PlayerMixin.java
@@ -6,8 +6,8 @@ import net.minecraft.world.entity.player.Player;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 import net.detectivekaktus.DefenseOfTheCraft;
 import net.detectivekaktus.component.DotcComponents;
@@ -17,22 +17,24 @@ import net.detectivekaktus.item.tool.HasBonusDamage;
 
 @Mixin(Player.class)
 public class PlayerMixin {
-    @Inject(
+    @ModifyExpressionValue(
             method = "attack",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z",
-                    shift = At.Shift.AFTER
+                    target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"
             )
     )
-    private void onAttack(Entity entity, CallbackInfo callbackInfo) {
+    private boolean onAttack(boolean hurt, Entity entity) {
+        if (!hurt)
+            return false;
+
         var player = (Player) (Object) this;
         if (player.level().isClientSide || !(player instanceof ServerPlayer))
-            return;
+            return hurt;
 
         var stack = player.getMainHandItem();
         if (!stack.has(DotcComponents.PROCABLE_COMPONENT))
-            return;
+            return hurt;
 
         var component = stack.get(DotcComponents.PROCABLE_COMPONENT);
         var chance = PseudoRandom.getProcChance(component.baseChance(), component.scale());
@@ -42,7 +44,7 @@ public class PlayerMixin {
                     //                   weirdly enough you can't do ++component.scale()
                     new ProcableComponent(component.baseChance(), component.scale() + 1)
             );
-            return;
+            return hurt;
         }
 
         stack.set(
@@ -60,5 +62,7 @@ public class PlayerMixin {
                     "{} has procable component but doesn't implement `HasBonusDamage` interface",
                     stack.getItem().toString()
             );
+
+        return hurt;
     }
 }
