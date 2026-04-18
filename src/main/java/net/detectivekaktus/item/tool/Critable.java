660df45e5a136524a56848ed53ef9a4d18e5840f
Artiom Astashonak <artiomastashonak@gmail.com>
2026-04-18 18:49:11 +0200

    core: add `Critable` interface

---
+++ src/main/java/net/detectivekaktus/item/tool/Critable.java
@@ -0,0 +1,5 @@
+package net.detectivekaktus.item.tool;
+
+public interface Critable {
+    float getCritPercent();
+}
--- src/main/java/net/detectivekaktus/item/tool/Crystalys.java
+++ src/main/java/net/detectivekaktus/item/tool/Crystalys.java
@@ -1,5 +1,6 @@
 package net.detectivekaktus.item.tool;
 
+import net.detectivekaktus.core.rng.PseudoRandomBaseChances;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
@@ -10,7 +11,9 @@ import java.util.List;
 
 import net.detectivekaktus.item.DotcItem;
 
-public class Crystalys extends SwordItem implements DotcItem {
+public class Crystalys extends SwordItem implements DotcItem, Critable {
+    private static final float CRIT_PERCENT = 1.33f;
+
     public Crystalys(Tier tier, Properties properties) {
         super(tier, properties);
     }
@@ -19,5 +22,10 @@ public class Crystalys extends SwordItem implements DotcItem {
     public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltip, TooltipFlag type) {
         var components = this.generateTooltipTranslationStrings(1, "crystalys");
         tooltip.addAll(components);
+    }
+
+    @Override
+    public float getCritPercent() {
+        return CRIT_PERCENT;
     }
 }
