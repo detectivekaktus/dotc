5c9734beb2895af2fc47af0ca4a4b5b8b444f9c7
Artiom Astashonak <artiomastashonak@gmail.com>
2026-05-03 12:01:00 +0200

    refactor: change creeper stun logic

--- src/main/java/net/detectivekaktus/mixin/entity/CreeperMixin.java
+++ src/main/java/net/detectivekaktus/mixin/entity/CreeperMixin.java
@@ -1,37 +1,22 @@
 package net.detectivekaktus.mixin.entity;
 
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.monster.Creeper;
-import net.minecraft.world.level.Level;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
-
 import net.detectivekaktus.effect.DotcEffects;
 
 @Mixin(Creeper.class)
 public class CreeperMixin {
-    @WrapWithCondition(
-            method = "explodeCreeper",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/Level;explode(Lnet/minecraft/world/entity/Entity;DDDFLnet/minecraft/world/level/Level$ExplosionInteraction;)Lnet/minecraft/world/level/Explosion;"
-            )
-    )
-    private boolean explodeIfNotStunned(Level level, Entity entity, double x, double y, double z, float radius, Level.ExplosionInteraction interaction) {
-        return !((Creeper) entity).hasEffect(DotcEffects.STUN);
-    }
-
     @Inject(
-            method = "spawnLingeringCloud",
+            method = "explodeCreeper",
             at = @At(value = "HEAD"),
             cancellable = true
     )
-    private void cancelCloud(CallbackInfo callbackInfo) {
+    private void explodeIfNotStunned(CallbackInfo callbackInfo) {
         var creeper = (Creeper) (Object) this;
         if (!creeper.hasEffect(DotcEffects.STUN))
             return;
